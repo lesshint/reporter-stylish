05db8707f8edeabf25305e42137ec1d553580f2f
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2017-01-05 12:49:31 +0100

    Rewrite gulpfile to ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
 
-gulp.task('lint', function () {
-    var eslint = require('gulp-eslint');
-
+gulp.task('lint', () => {
     return gulp.src(['./*.js'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('test', function () {
-    var mocha = require('gulp-mocha');
-
+gulp.task('test', () => {
     return gulp.src(['./test.js'])
         .pipe(mocha());
 });
